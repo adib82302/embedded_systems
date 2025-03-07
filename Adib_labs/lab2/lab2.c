9864c60fed88340f095f1d3bdeaa1256c54a6960
adib82302 <adib82302@gmail.com>
2025-03-07 01:46:42 -0500

    lab2.c draw cursor

--- Adib_labs/lab2/lab2.c
+++ Adib_labs/lab2/lab2.c
@@ -1,135 +1,148 @@
 /*
- * lab2.c: Main program for CSEE 4840 Lab 2
- * Implements VGA display, cursor management, and text input handling.
+ *
+ * CSEE 4840 Lab 2 for 2019
+ *
+ * Name/UNI: Kristian Nikolov (kdn2117)
+ * Name/UNI: Adib Khondoker (aak2250)
  */
-
 #include "fbputchar.h"
-#include "usbkeyboard.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include "usbkeyboard.h"
+#include <pthread.h>
+
+/* Update SERVER_HOST to be the IP address of
+ * the chat server you are connecting to
+ */
+/* arthur.cs.columbia.edu */
+#define SERVER_HOST "128.59.19.114"
+#define SERVER_PORT 42000
 
 #define BUFFER_SIZE 128
 #define TOP 0
 #define BOTTOM 23
 #define DIVIDE 15
+#define OUT 20
+#define	IN 8
 #define WIDTH 63
 
-#define INPUT_MAX 128
-#define INPUT_ROW (BOTTOM - 1)
+/*
+ * References:
+ *
+ * https://web.archive.org/web/20130307100215/http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+ *
+ * http://www.thegeekstuff.com/2011/12/c-socket-programming/
+ * 
+ */
 
-// Input buffer and cursor management
-char input_buffer[INPUT_MAX] = {0};
-int cursor_position = 0;
+int sockfd; /* Socket file descriptor */
 
-// Function Prototypes
-void update_input_display();
-void handle_keypress(char key);
-void display_message(const char *message);
-void clear_receive_area();
+struct libusb_device_handle *keyboard;
+uint8_t endpoint_address;
 
-int main() {
-    int err;
+pthread_t network_thread;
+void *network_thread_f(void *);
 
-    if ((err = fbopen()) != 0) {
-        fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
-        exit(1);
+int main()
+{
+  int err, col;
+
+  struct sockaddr_in serv_addr;
+
+  struct usb_keyboard_packet packet;
+  int transferred;
+  char keystate[12];
+
+  if ((err = fbopen()) != 0) {
+    fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
+    exit(1);
+  }
+
+  // Clear the screan
+  fbclear();
+
+  /* Draw rows of asterisks across the top and bottom of the screen */
+  for (col = 0 ; col < WIDTH ; col++) {
+    fbputchar('*', TOP, col);
+    fbputchar('-', DIVIDE, col);
+    fbputchar('*', BOTTOM, col);
+    fbputs(">", OUT, 0);
+  }
+
+  fbputs("Hello CSEE 4840 World!", 4, 10);
+
+  /* Open the keyboard */
+  if ( (keyboard = openkeyboard(&endpoint_address)) == NULL ) {
+    fprintf(stderr, "Did not find a keyboard\n");
+    exit(1);
+  }
+    
+  /* Create a TCP communications socket */
+  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+    fprintf(stderr, "Error: Could not create socket\n");
+    exit(1);
+  }
+
+  /* Get the server address */
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(SERVER_PORT);
+  if ( inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
+    fprintf(stderr, "Error: Could not convert host IP \"%s\"\n", SERVER_HOST);
+    exit(1);
+  }
+
+  /* Connect the socket to the server */
+  if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "Error: connect() failed.  Is the server running?\n");
+    exit(1);
+  }
+
+  /* Start the network thread */
+  pthread_create(&network_thread, NULL, network_thread_f, NULL);
+
+  /* Look for and handle keypresses
+   * Actually running the program
+   */
+  for (;;) {
+    libusb_interrupt_transfer(keyboard, endpoint_address,
+			      (unsigned char *) &packet, sizeof(packet),
+			      &transferred, 0);
+    if (transferred == sizeof(packet)) {
+      sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
+	      packet.keycode[1]);
+      printf("%s\n", keystate);
+      fbputs(keystate, OUT, 1);
+      if (packet.keycode[0] == 0x29) { /* ESC pressed? */
+	break;
+      }
     }
+  }
 
-    setup_screen();
+  /* Terminate the network thread */
+  pthread_cancel(network_thread);
 
-    /* Simulated input loop for testing */
-    while (1) {
-        handle_keypress('H'); // Simulates typing 'H'
-        handle_keypress('e'); // Simulates typing 'e'
-        handle_keypress('l'); // Simulates typing 'l'
-        handle_keypress('l'); // Simulates typing 'l'
-        handle_keypress('o'); // Simulates typing 'o'
-        handle_keypress('\b'); // Simulates backspace
-        handle_keypress('!'); // Simulates typing '!'
-        handle_keypress('\n'); // Simulates pressing Enter
+  /* Wait for the network thread to finish */
+  pthread_join(network_thread, NULL);
 
-        sleep(2); // Delay to observe changes on screen
-    }
-
-    return 0;
+  return 0;
 }
 
-/* Initialize the screen with borders and divider */
-void setup_screen() {
-    fbclear();
-    for (int col = 0; col < WIDTH; col++) {
-        fbputchar('*', TOP, col);       // Top border
-        fbputchar('-', DIVIDE, col);    // Middle divider
-        fbputchar('*', BOTTOM, col);    // Bottom border
-    }
-    fbputs(">", INPUT_ROW, 0); // Input prompt
+void *network_thread_f(void *ignored)
+{
+  char recvBuf[BUFFER_SIZE];
+  int n;
+  /* Receive data */
+  while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
+    recvBuf[n] = '\0';
+    printf("%s", recvBuf);
+    fbputs(recvBuf, IN, 0);
+  }
+
+  return NULL;
 }
 
-/* Update the input display with the cursor */
-void update_input_display() {
-    // Clear the input line
-    for (int col = 1; col < WIDTH; col++) {
-        fbputchar(' ', INPUT_ROW, col);
-    }
-
-    // Display the input buffer
-    fbputs(input_buffer, INPUT_ROW, 1);
-
-    // Draw the cursor as a vertical line '|'
-    fbputchar('|', INPUT_ROW, cursor_position + 1);
-}
-
-/* Handle raw keypress input for display and cursor management */
-void handle_keypress(char key) {
-    if (key == '\n') { // Enter key
-        display_message(input_buffer);
-        memset(input_buffer, 0, INPUT_MAX);
-        cursor_position = 0;
-    } 
-    else if (key == '\b') { // Backspace key
-        if (cursor_position > 0) {
-            cursor_position--;
-            input_buffer[cursor_position] = ' ';
-        }
-    } 
-    else { // Any other key
-        if (cursor_position < INPUT_MAX - 1) {
-            input_buffer[cursor_position++] = key;
-        }
-    }
-    update_input_display();
-}
-
-/* Display a message in the top section of the screen */
-void display_message(const char *message) {
-    static int row = 1;
-    int col = 0;
-    const char *p = message;
-
-    while (*p) {
-        if (*p == '\n' || col >= WIDTH) {
-            row++;
-            col = 0;
-        }
-        if (row >= DIVIDE) {
-            row = 1;
-            clear_receive_area();
-        }
-        if (*p != '\n') {
-            fbputchar(*p, row, col++);
-        }
-        p++;
-    }
-    row++;
-}
-
-/* Clear only the receive area */
-void clear_receive_area() {
-    for (int row = 1; row < DIVIDE; row++) {
-        for (int col = 0; col < WIDTH; col++) {
-            fbputchar(' ', row, col);
-        }
-    }
-}
