40b2c823458acf2542715644f2bf3edd3250d024
adib82302 <adib82302@gmail.com>
2025-03-07 06:20:16 -0500

    arrow_keys_v3

--- Adib_labs/lab2/lab2.c
+++ Adib_labs/lab2/lab2.c
@@ -219,7 +219,9 @@ void update_input_display() {
 
 /* Handle keypresses, store in buffer, and update display */
 void handle_keypress(const char *keystate, char ascii_char) {
-    uint8_t keycode = (uint8_t)strtol(keystate + 14, NULL, 16);
+    uint8_t keycode = packet.keycode[0];
+
+    //uint8_t keycode = (uint8_t)strtol(keystate + 14, NULL, 16);
     if (ascii_char == '\n') { // Enter key
         char message_to_send[BUFFER_SIZE] = {0};
         int msg_length = 0;
