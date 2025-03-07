6de0e122a8be24b9498c384536e066e460cbd7cf
adib82302 <adib82302@gmail.com>
2025-03-07 15:28:58 -0500

    fix file

--- Adib_labs/lab2/lab2.c
+++ Adib_labs/lab2/lab2.c
@@ -17,6 +17,9 @@
 #include <time.h>
 #include <unistd.h>
 
+#define LEFT_ARROW 0x50  // Example keycode for Left Arrow
+#define RIGHT_ARROW 0x4F  // Example keycode for Right Arrow
+
 
 #define REPEAT_DELAY 300000 // Initial delay (300ms) before repeating
 #define REPEAT_RATE 50000   // Repeat rate (50ms) for continued input
